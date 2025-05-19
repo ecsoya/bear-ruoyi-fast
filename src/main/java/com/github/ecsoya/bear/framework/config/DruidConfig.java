6604d90c0b6e92f04bbc2b0b9715ba8492a82343
AngryRED <angryred@qq.com>
2025-05-19 11:17:05 +0800

    Druid优化

--- src/main/java/com/github/ecsoya/bear/framework/config/DruidConfig.java
+++ src/main/java/com/github/ecsoya/bear/framework/config/DruidConfig.java
@@ -20,7 +20,6 @@ import org.springframework.context.annotation.Primary;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
-import com.alibaba.druid.spring.boot.autoconfigure.properties.DruidStatProperties;
 import com.alibaba.druid.util.Utils;
 import com.github.ecsoya.bear.common.utils.spring.SpringUtils;
 import com.github.ecsoya.bear.framework.aspectj.lang.enums.DataSourceType;
@@ -78,14 +77,8 @@ public class DruidConfig {
 	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Bean
-	@ConditionalOnProperty(name = "spring.datasource.druid.statViewServlet.enabled", havingValue = "true")
-	public FilterRegistrationBean removeDruidFilterRegistrationBean(DruidStatProperties properties) {
-		// 获取web监控页面的参数
-		DruidStatProperties.StatViewServlet config = properties.getStatViewServlet();
-		// 提取common.js的配置路径
-		String pattern = config.getUrlPattern() != null ? config.getUrlPattern() : "/druid/*";
-		String commonJsPattern = pattern.replaceAll("\\*", "js/common.js");
-		final String filePath = "support/http/resources/js/common.js";
+	@ConditionalOnProperty(name = "spring.datasource.druid.stat-view-servlet.enabled", havingValue = "true")
+	public FilterRegistrationBean removeDruidFilterRegistrationBean() {
 		// 创建filter进行过滤
 		Filter filter = new Filter() {
 			@Override
@@ -99,7 +92,7 @@ public class DruidConfig {
 				// 重置缓冲区，响应头不会被重置
 				response.resetBuffer();
 				// 获取common.js
-				String text = Utils.readFromResource(filePath);
+				String text = Utils.readFromResource("support/http/resources/js/common.js");
 				// 正则替换banner, 除去底部的广告信息
 				text = text.replaceAll("<a.*?banner\"></a><br/>", "");
 				text = text.replaceAll("powered.*?shrek.wang</a>", "");
@@ -112,7 +105,7 @@ public class DruidConfig {
 		};
 		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
 		registrationBean.setFilter(filter);
-		registrationBean.addUrlPatterns(commonJsPattern);
+		registrationBean.addUrlPatterns("/druid/js/common.js");
 		return registrationBean;
 	}
 }
